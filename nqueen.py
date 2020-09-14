dbfeb9f6fb60c96db532d0cc28907efe47eba0c4
Juan Oyosa <antnio.dmora@gmail.com>
2020-09-14 07:13:43 -0500

    Adds sql alchemy and stores nqueen data

--- nqueen.py
+++ nqueen.py
@@ -1,13 +1,30 @@
 from itertools import permutations
+import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class NQueen(Base):
+    __tablename__ = "nqueen"
+    id = Column(Integer, primary_key=True)
+    n = Column(Integer)
+    solution_count = Column(Integer)
+
+    def __repr__(self):
+        return "Solutions for N={}: {}".format(str(self.n), str(self.solution_count))
+
 
 def find_nqueen(n=8, display=True):
     if n<8:
         raise Exception('n must be grater than or equal 8')
-    sol=0
+    sol = 0
     cols = range(n)
     result = []
     for combo in permutations(cols):                      
-        if n==len(set(combo[i]+i for i in cols))==len(set(combo[i]-i for i in cols)):
+        if n == len(set(combo[i]+i for i in cols)) == len(set(combo[i]-i for i in cols)):
             sol += 1
             result.append(combo)
             if display:
@@ -17,6 +34,24 @@ def find_nqueen(n=8, display=True):
     return (sol, result)
 
 if __name__ == '__main__':
-    for i in range(8,11):
-        solution_count, solutions = find_nqueen(i, False)
-        print("Total solutions for N={}: {}".format(i, str(solution_count)))
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWD = os.environ.get("DB_PASSWD")
+
+    engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWD}@{DB_HOST}/{DB_NAME}")
+
+    if not Base.metadata.tables[NQueen.__tablename__].exists(engine):
+        Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    for i in range(8,13):
+        solution_count, solution_set = find_nqueen(i, False)
+        session = Session()
+        n_queen = NQueen(n=i, solution_count=solution_count)
+        session.add(n_queen)
+        session.commit()
+        print(n_queen)
+
+
